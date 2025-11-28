8202e62789a3646e38f051ae272894799231fe21
LE93 <LE93@users.noreply.github.com>
2025-11-28 13:47:08 +0000

    updated flask_app

--- flask_app.py
+++ flask_app.py
@@ -49,7 +49,7 @@ def webhook():
 # Auth routes
 @app.get("/users")
 def users():
-    return render_template("users.html");
+    return render_template("users.html", title="Users", users=["User a", "User b"]);
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
