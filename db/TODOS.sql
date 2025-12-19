93270db293259d580d04995b970ba285f0c4fb85
LE93 <LE93@users.noreply.github.com>
2025-12-19 06:58:40 +0000

    Revert db.

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -1,15 +1,13 @@
-/*
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(250) NOT NULL UNIQUE,
     password VARCHAR(250) NOT NULL
 );
-*/
 
-CREATE TABLE product (
+CREATE TABLE todos (
     id INT AUTO_INCREMENT PRIMARY KEY,
-   	owner_id INT NOT NULL,
-    title VARCHAR(100),
-    preis DECIMAL(8,2),
-    FOREIGN KEY (owner_id) REFERENCES users(id)
+    user_id INT NOT NULL,
+    content VARCHAR(100),
+    due DATETIME,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
